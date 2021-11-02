0f012db9f4ea66b33d1f96cf47d28c75c38a4ac9
Danil <danilunrandom@gmail.com>
2021-11-02 03:54:21 +0000

    refactor: reformated Map2D as function component

--- components/Map2D/index.tsx
+++ components/Map2D/index.tsx
@@ -10,52 +10,35 @@ type MapProps = {
   markers?: Markers;
 };
 
-type MapState = {};
-
-class Map2D extends Component<MapProps, MapState> {
-  constructor(props: MapProps) {
-    super(props);
-    this.state = {} as MapState;
-
-    // uncomment if u r using this in handler
-    // this.onMarkerClick = this.onMarkerClick.bind(this);
-  }
-
-  onMarkerClick(place_id: string) {
-    return (event: MouseEvent) => {
-      console.log(`marker of place ${place_id} was clicked`);
-    };
-  }
-
-  render() {
-    const {
-      APIkey,
-      zoom = 0,
-      center = { lng: 0, lat: 0 },
-      markers = [],
-    } = this.props;
-
-    return (
-      <GoogleMapReact
-        bootstrapURLKeys={{
-          key: APIkey,
-        }}
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        {markers.map((marker: Marker) => (
-          <Marker
-            // rendering stuff
-            key={marker.place_id}
-            // position
-            {...marker.pos}
-            name={marker.name}
-            onClick={this.onMarkerClick(marker.place_id)}
-          />
-        ))}
-      </GoogleMapReact>
-    );
-  }
+function onMarkerClick(place_id: string) {
+  return (event: MouseEvent) => {
+    console.log(`marker of place ${place_id} was clicked`);
+  };
 }
+
+const Map2D = (props: MapProps) => {
+  const { APIkey, zoom = 0, center = { lng: 0, lat: 0 }, markers = [] } = props;
+
+  return (
+    <GoogleMapReact
+      bootstrapURLKeys={{
+        key: APIkey,
+      }}
+      defaultCenter={center}
+      defaultZoom={zoom}
+    >
+      {markers.map((marker: Marker) => (
+        <Marker
+          // rendering stuff
+          key={marker.place_id}
+          // position
+          {...marker.pos}
+          name={marker.name}
+          onClick={onMarkerClick(marker.place_id)}
+        />
+      ))}
+    </GoogleMapReact>
+  );
+};
 
 export default Map2D;
