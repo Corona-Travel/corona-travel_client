7d3a4b926b13855adafc5f139ac53f09e8e38b9a
unrndm <unrndm@gmail.com>
2021-12-16 16:56:08 +0300

    chore: removed old config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_GMAP_KEY: process.env.NEXT_PUBLIC_GMAP_KEY,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  }
 };
