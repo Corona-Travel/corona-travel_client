437a61d929b862be42f5ae0658974a361f4718f3
unrndm <unrndm@gmail.com>
2021-12-16 05:44:13 +0300

    chore: updated next.config gile

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_GMAP_KEY: process.env.NEXT_PUBLIC_GMAP_KEY,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  }
 };
